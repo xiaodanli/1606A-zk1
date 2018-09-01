f4f453bd61b2098048d4e840b1a8fc810e8789b8
lixd <dandan580826@163.com>
2018-09-01 09:14:02 +0800

    gulp任务搭建完毕，初始完成

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,18 @@ var sass = require('gulp-sass');
 
 var minCss = require('gulp-clean-css');
 
+var uglify = require('gulp-uglify');
+
+//起服务   gulp-webserver
+
+var server = require('gulp-webserver');
+
+var url = require('url');
+
+var fs = require('fs');
+
+var path = require('path');
+
 //编译scss压缩css
 gulp.task('devCss', function() {
     return gulp.src('./src/scss/*.scss')
@@ -18,5 +30,35 @@ gulp.task('watch', function() {
     return gulp.watch('./src/scss/*.scss', gulp.series('devCss'))
 })
 
+//压缩js
+
+gulp.task('minJs', function() {
+    return gulp.src(['./src/js/**/*.js', '!./src/js/libs/*.js'])
+        .pipe(uglify())
+        .pipe(gulp.dest('build'))
+})
+
+
+gulp.task('devServer', function() {
+    return gulp.src('src')
+        .pipe(server({
+            port: 9090, //配置端口
+            middleware: function(req, res, next) { //拦截前端请求
+                var pathname = url.parse(req.url).pathname;
+                if (pathname === '/favicon.ico') {
+                    res.end('');
+                    return
+                }
+
+                console.log(pathname);
+
+                pathname = pathname === '/' ? '/index.html' : pathname;
+
+                res.end(fs.readFileSync(path.join(__dirname, 'src', pathname)))
+
+            }
+        }))
+})
+
 //整合任务
-gulp.task('dev', gulp.series('devCss', 'watch'))
+gulp.task('dev', gulp.series('devCss', 'minJs', 'devServer', 'watch'))
